7e3061240b554b997a149fb23db80e49e59dce74
Romain J <rom1.jaldo@gmail.com>
2018-11-16 11:26:11 +0100

    fix #8 controller + views for eventCategories

--- src/LIV/AppBundle/Controller/DefaultController.php
+++ src/LIV/AppBundle/Controller/DefaultController.php
@@ -10,7 +10,6 @@ class DefaultController extends Controller
     {
         return $this->render('@LIVApp/Default/index.html.twig');
     }
-
     public function getPlacesCategories()
     {
         $em = $this->getDoctrine()->getManager();
@@ -18,6 +17,14 @@ class DefaultController extends Controller
 
         return $this->render('@LIVApp/Default/places-categories-dropdown.html.twig', array(
             "placeCategories" => $placeCategories
+    }
+
+    public function getEventsCategories()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $eventCategories = $em->getRepository('LIVAppBundle:EventCategory')->findAll();
+        return $this->render('@LIVApp/Default/events-categories-dropdown.html.twig', array(
+            "eventCategories" => $eventCategories
         ));
     }
 }
