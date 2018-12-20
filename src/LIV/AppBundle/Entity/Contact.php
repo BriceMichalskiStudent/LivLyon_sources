b2eab42356a0b24a7d08d33c6f1dbfa8c841a5ca
jaldo <rom1.jaldo@gmail.com>
2018-12-20 16:46:28 +0100

    #15 render

---
+++ src/LIV/AppBundle/Entity/Contact.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaldo
+ * Date: 20/12/18
+ * Time: 15:03
+ */
+
+namespace LIV\AppBundle\Entity;
+
+
+class Contact
+{
+    private $id;
+    private $firstname;
+    private $name;
+    private $email;
+    private $object;
+    private $message;
+    private $submit;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * @param mixed $object
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubmit()
+    {
+        return $this->submit;
+    }
+
+    /**
+     * @param mixed $submit
+     */
+    public function setSubmit($submit)
+    {
+        $this->submit = $submit;
+    }
+
+
+}
