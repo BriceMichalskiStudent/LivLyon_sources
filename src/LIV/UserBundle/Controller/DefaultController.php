4d1eefc489c8a97a03fdfecee01d59cb271dc3d8
Brice MICHALSKI <brice.michalski@icloud.com>
2019-01-22 15:16:30 +0100

    firewall / encoders / providers : FOSUserBundle configuration

--- src/LIV/UserBundle/Controller/DefaultController.php
+++ src/LIV/UserBundle/Controller/DefaultController.php
@@ -3,13 +3,10 @@
 namespace LIV\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
+
     public function indexAction()
     {
         return $this->render('LIVUserBundle:Default:index.html.twig');
